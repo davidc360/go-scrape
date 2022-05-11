57aa181cbab8a7a30cd5bf518e91805ea7cdeb69
David Cao <davidcao@Davids-MBP-16.home>
2022-05-11 19:05:33 -0400

    prints out the most popular company

--- scrape.go
+++ scrape.go
@@ -7,9 +7,21 @@ import (
 
 func main() {
 	c := colly.NewCollector()
+
+	var tickers []string
+	var companyNames []string
+
 	c.OnHTML(".simpTblRow > td > a", func(e *colly.HTMLElement) {
-		fmt.Printf("Most popular ticker: %s\n", e.Text)
+		// fmt.Printf("Most popular ticker: %s\n", e.Text)
+		tickers = append(tickers, e.Text)
+	})
+
+	c.OnHTML("[aria-label=\"Name\"]", func(e *colly.HTMLElement) {
+		// fmt.Printf("Most popular company: %s\n", e.Text)
+		companyNames = append(companyNames, e.Text)
 	})
 
 	c.Visit("https://finance.yahoo.com/trending-tickers")
+
+	fmt.Printf("The most popular ticker is $%s, %s\n", tickers[0], companyNames[0])
 }
